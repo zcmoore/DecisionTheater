2b840302c92ec207782e2cede08b74624fe67a41
Zach Moore <zcmoore@asu.edu>
2015-06-03 19:56:44 -0700

    Initial client

---
+++ public/source/client.js
@@ -0,0 +1,71 @@
+var name;
+var socket;
+var hasControl;
+
+function startClient() {
+  hasControl = false;
+  requestName();
+  connectToServer();
+  $("#notifications").append('Logged in as <b><u>' + name + '</u></b>');
+}
+
+function connectToServer() {
+  socket = io.connect('/');
+
+  socket.on('connect', function() {
+    // serverside call
+    socket.emit('adduser', name);
+  });
+
+  socket.on('updateusers', function(data) {
+    $('#users').empty();
+    $.each(data, function(key, value) {
+      $('#users').append('<div>' + key + '</div>');
+    });
+  });
+
+  socket.on('controlgrant', function(data) {
+    hasControl = data.granted;
+    alert(hasControl);
+  });
+
+  socket.on('cameraUpdate', function(data) {
+    onCameraUpdate(data);
+  });
+
+  socket.on('servernotification', function(message) {});
+}
+
+function requestName() {
+  name = window.prompt("Please enter your name", "");
+  if (name.trim().length === 0) {
+    alert("Name required");
+    requestName();
+  } else if (name === null || name == "null") {
+    $('#invalidLogin').show();
+    $('#validLogin').hide();
+  } else {
+    $('#validLogin').show();
+    $('#invalidLogin').hide();
+  }
+}
+
+function sendUpdatedCameraInformation(cameraData)
+{
+  socket.emit('cameraUpdate', cameraData);
+}
+
+function relinquishControls() {
+    hasControl = false;
+    socket.emit('relinquishControl');
+}
+
+function requestControls() {
+  /*
+    var message = {
+        type: "requestControl"
+    };
+    var jsonMessage = JSON.stringify(message);
+    socket.send(jsonMessage);
+    */
+}
---
+++ public/source/render-app.js
@@ -0,0 +1,125 @@
+window.addEventListener('resize', onWindowResize, false);
+
+var camera, scene, renderer;
+var cameraControls, effectController;
+var clock = new THREE.Clock();
+
+function updateCamera() {
+    var position_x = camera.position.x;
+    var position_y = camera.position.y;
+    var position_z = camera.position.z;
+    var target_x = cameraControls.target.x;
+    var target_y = cameraControls.target.y;
+    var target_z = cameraControls.target.z;
+
+    var cameraData = {
+        position_x: position_x,
+        position_y: position_y,
+        position_z: position_z,
+        target_x: target_x,
+        target_y: target_y,
+        target_z: target_z
+    };
+
+    sendUpdatedCameraInformation(cameraData);
+}
+
+function onCameraUpdate(cameraData) {
+  camera.position.x = cameraData.position_x;
+  camera.position.y = cameraData.position_y;
+  camera.position.z = cameraData.position_z;
+  cameraControls.target.x = cameraData.target_x;
+  cameraControls.target.y = cameraData.target_y;
+  cameraControls.target.z = cameraData.target_z;
+}
+
+function onWindowResize() {
+  var canvasWidth = window.innerWidth * .75;
+  var canvasHeight = window.innerHeight - $('#jsviewport').offset().top;
+
+  renderer.setSize(canvasWidth, canvasHeight);
+
+  camera.aspect = canvasWidth / canvasHeight;
+  camera.updateProjectionMatrix();
+}
+
+function init() {
+  var canvasWidth = window.innerWidth * .75;
+  var canvasHeight = window.innerHeight - $('#jsviewport').offset().top;
+  var canvasRatio = canvasWidth / canvasHeight;
+
+  // RENDERER
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
+  renderer.setSize(canvasWidth, canvasHeight);
+  renderer.setClearColorHex(0x888888, 1.0);
+
+  // CAMERA
+  camera = new THREE.PerspectiveCamera(45, canvasRatio, 1, 40000);
+
+  // CONTROLS
+  cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
+
+  camera.position.set(-480, 659, -619);
+  cameraControls.target.set(4, 301, 92);
+
+  // SCENE
+  fillScene();
+}
+
+function fillScene() {
+  // SCENE
+  scene = new THREE.Scene();
+  scene.fog = new THREE.Fog(0x808080, 3000, 6000);
+  // LIGHTS
+  var ambientLight = new THREE.AmbientLight(0x222222);
+  var light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+  light.position.set(200, 400, 500);
+
+  var light2 = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+  light2.position.set(-400, 200, -300);
+
+  scene.add(ambientLight);
+  scene.add(light);
+  scene.add(light2);
+  Coordinates.drawGround({
+    size: 1000
+  });
+  createSnowman(0, 0, 0);
+}
+
+function addToDOM() {
+  var container = document.getElementById('jsviewport');
+  var canvas = container.getElementsByTagName('canvas');
+  if (canvas.length > 0) {
+    container.removeChild(canvas[0]);
+  }
+  container.appendChild(renderer.domElement);
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  var delta = clock.getDelta();
+  cameraControls.enabled = hasControl;
+  cameraControls.update(delta);
+  updateCamera();
+  renderer.render(scene, camera);
+}
+
+function startRenderApp() {
+  try {
+    init();
+    addToDOM();
+    animate();
+  } catch (e) {
+    var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
+    $('#container').append(errorReport + e);
+  }
+}
