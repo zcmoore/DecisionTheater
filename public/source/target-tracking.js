3c9b8ba6dd68d8c6730ad0f3b17a60c7c9fc341d
Zach Moore <zcmoore@asu.edu>
2015-06-30 08:33:55 -0700

    Tracker structure

---
+++ public/source/target-tracking.js
@@ -0,0 +1,10 @@
+TRACKING.TrackingTarget = function( sourceObject ) {
+
+	// Object in the scene that is being tracked
+	this.source = sourceObject;
+
+	this.category = "NONE";
+
+	this.tagIndex = -1;
+
+};
